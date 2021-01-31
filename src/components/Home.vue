fd36c7b666e1b7c87656fcaaa9ffff18ac485f33
mghboy45 <3170492917@qq.com>
2021-01-31 21:57:54 +0800

    main and menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,116 @@
 <template>
-  <div>
-    <el-button type='info' @click='logout'>退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 内容区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class='toggle-button' @click='toggleCollapse'>
+          <span>|||</span>
+        </div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :collapse='isCollapse'
+          router
+        >
+          <el-submenu :index="item.id + ''" v-for='item in menuList' :key='item.id'>
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item :index="'/' + child.path" v-for='child in item.children' :key='child.id'>
+              <i class="el-icon-menu"></i>
+              <span>{{child.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右边内容区 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      menuList: [],
+      isCollapse: false,
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      }
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.removeItem('token')
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) {
+        return this.$message.error('res.meta.msg')
+      }
+      console.log(res.data)
+      this.menuList = res.data
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
 <style>
-
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-menu {
+  border-right: none !important;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  padding-right: 10px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
